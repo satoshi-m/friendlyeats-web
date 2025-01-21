21ac5774464ab07fe05d90ff7290cca3df139cdf
Satoshi Mimura <mimura@smegandsynaps.jp>
2025-01-21 23:12:27 +0900

    レストランのリアルタイム更新をリッスン. ユーザーがレストランのレビューを送信できるようにする

--- src/app/actions.js
+++ src/app/actions.js
@@ -10,6 +10,10 @@ import { getFirestore } from "firebase/firestore";
 // This is a next.js server action, which is an alpha feature, so
 // use with caution.
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
+
+// This is a next.js server action, which is an alpha feature, so
+// use with caution.
+// https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
 export async function handleReviewFormSubmission(data) {
   const { app } = await getAuthenticatedAppForUser();
   const db = getFirestore(app);
