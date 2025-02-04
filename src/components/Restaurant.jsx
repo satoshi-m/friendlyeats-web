67c575f422c6e4543e2793ca62566242f4c6d4a2
Satoshi Mimura <mimura@smegandsynaps.jp>
2025-02-04 11:02:19 +0900

    ユーザーが各レストランの写真を変更できるようにする

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -34,15 +34,30 @@ export default function Restaurant({
     setReview({ ...review, [name]: value });
   };
 
-  async function handleRestaurantImage(target) {
-    const image = target.files ? target.files[0] : null;
-    if (!image) {
-      return;
-    }
+  export async function updateRestaurantImage(restaurantId, image) {
+    try {
+            if (!restaurantId)
+                    throw new Error("No restaurant ID has been provided.");
 
-    const imageURL = await updateRestaurantImage(id, image);
-    setRestaurantDetails({ ...restaurant, photo: imageURL });
-  }
+            if (!image || !image.name)
+                    throw new Error("A valid image has not been provided.");
+
+            const publicImageUrl = await uploadImage(restaurantId, image);
+            await updateRestaurantImageReference(restaurantId, publicImageUrl);
+
+            return publicImageUrl;
+    } catch (error) {
+            console.error("Error processing request:", error);
+    }
+}
+
+async function uploadImage(restaurantId, image) {
+    const filePath = `images/${restaurantId}/${image.name}`;
+    const newImageRef = ref(storage, filePath);
+    await uploadBytesResumable(newImageRef, image);
+
+    return await getDownloadURL(newImageRef);
+}
 
   const handleClose = () => {
     setIsOpen(false);
